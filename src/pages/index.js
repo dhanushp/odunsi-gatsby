8d5cb4ad3c82195d0a0c77017b53fd9dd5e593c1
dhanushp <dhanushp.work@gmail.com>
2020-04-06 21:37:34 +0530

    Added Contents and Style

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import "../styles/styles.scss"
 //Import components
 import Header from "../components/header"
 import Banner from "../components/banner"
+import AboutBlurb from "../components/aboutBlurb"
 
 const IndexPage = () => (
   <div>
     <Header />
     <Banner />
+    <AboutBlurb/>
   </div>
     
 
